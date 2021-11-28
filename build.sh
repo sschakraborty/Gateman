c9182c10b24f795f3ee3b7e8585d78b87638771a
sschakraborty <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>
2021-11-28 11:21:52 +0530

    Improved build script!

--- build.sh
+++ build.sh
@@ -5,9 +5,14 @@ RUSTC_VERSION=$($RUSTC_LOC --version)
 
 BUILD_MODE="debug"
 CARGO_BUILD_FLAG=""
+if [ -n "$1" ]; then
+  if [ "$1" == "release" ]; then
+    BUILD_MODE="release"
+  fi
+fi
 if [ "$BUILD_MODE" == "release" ]; then
   CARGO_BUILD_FLAG="--release"
-fi;
+fi
 
 echo "Cargo found at $CARGO_LOC"
 echo "Rust compiler at $RUSTC_LOC"
@@ -18,10 +23,10 @@ echo "Build mode: $BUILD_MODE"
 BASE_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 echo "Base directory: $BASE_DIR"
 
-/bin/bash -c "cd $BASE_DIR && $CARGO_LOC fmt" > "$BASE_DIR"/build.log 2>&1
-/bin/bash -c "cd $BASE_DIR && $CARGO_LOC build $CARGO_BUILD_FLAG" > "$BASE_DIR"/build.log 2>&1
+/bin/bash -c "cd $BASE_DIR && $CARGO_LOC fmt" >"$BASE_DIR"/build.log 2>&1
+/bin/bash -c "cd $BASE_DIR && $CARGO_LOC build $CARGO_BUILD_FLAG" >"$BASE_DIR"/build.log 2>&1
 
 mkdir -p "$BASE_DIR"/build
-cp -vuf "$BASE_DIR"/target/"$BUILD_MODE"/Gateman "$BASE_DIR"/build/
+cp -vf "$BASE_DIR"/target/"$BUILD_MODE"/Gateman "$BASE_DIR"/build/
 chmod go-rwx "$BASE_DIR"/build/Gateman
 chmod ug+rx "$BASE_DIR"/build/Gateman
