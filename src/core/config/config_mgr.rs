451d9e0c1492d7fe1e3653bb0ef3767c413b6355
sschakraborty <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>
2021-11-30 09:20:23 +0530

    Improved config manager handling of responses!

--- src/core/config/config_mgr.rs
+++ src/core/config/config_mgr.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::sync::Arc;
 
 use tokio::sync::mpsc::Receiver;
@@ -8,12 +9,11 @@ use crate::configuration_reader::origin_def_reader::Origin;
 use crate::core::config::read_config::{read_all_api_definitions, read_all_origin_definitions};
 use crate::{ConfigMgrProxyAPI, RateLimiterAPI};
 
-async fn initialize(
+async fn send_origin_definitions_to_rate_limiter(
     rate_limiter_tx: tokio::sync::mpsc::Sender<RateLimiterAPI>,
-) -> (Arc<Vec<APIDefinition>>, Arc<Vec<Origin>>) {
-    let api_definitions = Arc::new(read_all_api_definitions());
-    let origin_definitions = Arc::new(read_all_origin_definitions());
-    for origin_def in origin_definitions.as_ref() {
+    origin_definitions: &Vec<Origin>,
+) {
+    for origin_def in origin_definitions {
         rate_limiter_tx
             .send(RateLimiterAPI::UpdateOriginSpecification {
                 origin_id: origin_def.origin_id.clone(),
@@ -21,43 +21,33 @@ async fn initialize(
             })
             .await;
     }
-    (api_definitions, origin_definitions)
 }
 
-pub(crate) async fn deploy_config_mgr(
-    mut receiver: Receiver<ConfigMgrProxyAPI>,
+async fn initialize(
     rate_limiter_tx: tokio::sync::mpsc::Sender<RateLimiterAPI>,
-) {
-    let (api_definitions, origin_definitions) = initialize(rate_limiter_tx).await;
-    loop {
-        let api_call = receiver.recv().await;
-        if api_call.is_some() {
-            let api_call = api_call.unwrap();
-            let api_definitions = api_definitions.clone();
-            let origin_definitions = origin_definitions.clone();
-            tokio::spawn(async move {
-                match api_call {
-                    ConfigMgrProxyAPI::GetAPIDefinitionBySpecification {
-                        specification,
-                        responder,
-                    } => get_api_def_by_specification(specification, responder, api_definitions),
-                    ConfigMgrProxyAPI::GetOriginDefinitionByID {
-                        origin_id,
-                        responder,
-                    } => get_origin_def_by_id(origin_id, responder, origin_definitions),
-                }
-            });
-        }
+) -> (HashMap<String, APIDefinition>, HashMap<String, Origin>) {
+    let api_definitions = read_all_api_definitions();
+    let origin_definitions = read_all_origin_definitions();
+    let mut api_def_map = HashMap::new();
+    let mut origin_def_map = HashMap::new();
+    send_origin_definitions_to_rate_limiter(rate_limiter_tx, &origin_definitions).await;
+    for api_def in api_definitions {
+        api_def_map.insert(api_def.api_id.clone(), api_def);
     }
+    for origin_def in origin_definitions {
+        origin_def_map.insert(origin_def.origin_id.clone(), origin_def);
+    }
+    (api_def_map, origin_def_map)
 }
 
 fn get_api_def_by_specification(
     query_specification: APISpecification,
     responder: Sender<Option<APIDefinition>>,
-    api_definitions: Arc<Vec<APIDefinition>>,
+    api_definitions: Arc<HashMap<String, APIDefinition>>,
 ) {
     let mut matching_path_api_def_vec: Vec<&APIDefinition> = vec![];
     for api_definition in api_definitions.as_ref() {
+        let api_definition = api_definition.1;
         let api_specification = &(api_definition.specification);
         for query_path in &query_specification.paths {
             for api_path in &api_specification.paths {
@@ -97,14 +87,41 @@ fn get_api_def_by_specification(
 fn get_origin_def_by_id(
     origin_id: String,
     responder: Sender<Option<Origin>>,
-    origin_definitions: Arc<Vec<Origin>>,
+    origin_definitions: Arc<HashMap<String, Origin>>,
 ) {
-    let mut origin_def_selected: Option<Origin> = Option::None;
-    let id_ref_to_find = &origin_id;
-    for origin_definition in origin_definitions.as_ref() {
-        if origin_definition.has_id(id_ref_to_find) {
-            origin_def_selected = Option::Some(origin_definition.clone());
+    responder.send(
+        origin_definitions
+            .as_ref()
+            .get(&*origin_id)
+            .map(|origin| origin.clone()),
+    );
+}
+
+pub(crate) async fn deploy_config_mgr(
+    mut receiver: Receiver<ConfigMgrProxyAPI>,
+    rate_limiter_tx: tokio::sync::mpsc::Sender<RateLimiterAPI>,
+) {
+    let (api_definitions, origin_definitions) = initialize(rate_limiter_tx).await;
+    let api_definitions = Arc::new(api_definitions);
+    let origin_definitions = Arc::new(origin_definitions);
+    loop {
+        let api_call = receiver.recv().await;
+        if api_call.is_some() {
+            let api_call = api_call.unwrap();
+            let api_definitions = api_definitions.clone();
+            let origin_definitions = origin_definitions.clone();
+            tokio::spawn(async move {
+                match api_call {
+                    ConfigMgrProxyAPI::GetAPIDefinitionBySpecification {
+                        specification,
+                        responder,
+                    } => get_api_def_by_specification(specification, responder, api_definitions),
+                    ConfigMgrProxyAPI::GetOriginDefinitionByID {
+                        origin_id,
+                        responder,
+                    } => get_origin_def_by_id(origin_id, responder, origin_definitions),
+                }
+            });
         }
     }
-    responder.send(origin_def_selected);
 }
